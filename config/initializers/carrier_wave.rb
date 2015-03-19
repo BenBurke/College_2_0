5144e8e7e0e9bb626b8971983a75e6c3c32e4de8
Ben Burke <benjamin.burke@yale.edu>
2015-03-19 16:39:29 -0400

    trying to get heroku to work

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,10 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJFSOWJNAFUX3PTJA'],
-      :aws_secret_access_key => ENV['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']
-    }
+    :provider               => 'AWS',
+    :aws_access_key_id      => Rails.configuration.s3_access_key_id,
+    :aws_secret_access_key  => Rails.configuration.s3_secret_access_key,
+  }
     config.fog_directory     =  ENV['college2.0']
   end
 end
